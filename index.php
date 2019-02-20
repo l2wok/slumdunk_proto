650e81fd1638c9c4c89ee231e22878795c0fbd3d
Andro <l22wok@gmail.com>
2019-02-21 00:48:16 +0300

    add

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
         <meta name="author" content="">
 
         <title>MainPage proto</title>
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png"/>
+        
         <!-- // css block -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
