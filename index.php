516acfdffd2b9e1f09c68a7da8fd85bd85c157a4
Andro <l22wok@gmail.com>
2019-02-21 16:27:01 +0300

    1
    1

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
         <!-- // css block -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/proto_styles1.css">
+        <link rel="stylesheet" href="assets/css/proto_styles7.css">
 
     </head>
     <body>
         <!-- ~~~~~~ HEADER ~~~~~ -->
         <header class="">
-            <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg fixed-top sticky-top navbar-light bg-light">
                 <div class="container d-flex align-items-end">
                     <a class="navbar-brand mb-1" href="#">
                         <div class="logo">
