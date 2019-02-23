9a0b488addb47d06ef43219b628f60c8788fa8c8
Andro <l22wok@gmail.com>
2019-02-23 09:43:14 +0300

    end day

--- second.php
+++ second.php
@@ -13,8 +13,103 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/sec_styles.css">
     </head>
-    <body>
-        
+    <body class="d-flex flex-column h-100">
+        <header>
+            <div class="orange_shadow"></div>
+            <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+                <div class="container top-nav d-flex align-items-center">
+                    <a class="d-md-none d-lg-block mr-auto mr-lg-1 btn btn-outline-dark mr-1 " href="#"><i class="fa fa-home"></i></a>
+                    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse offcanvas-collapse" id="j-nav">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item my-md-1 my-lg-0 mr-lg-1 active">
+                                <a class="btn btn-outline-dark my-md-1 my-sm-0" href="#">Dashboard</a>
+                            </li>
+                            <li class="nav-item my-md-1 my-lg-0 mr-lg-1">
+                                <a class="btn btn-outline-dark my-md-1 my-sm-0" href="#">Dashboard</a>
+                            </li>
+                            <li class="nav-item my-md-1 my-lg-0 mr-lg-1 dropdown">
+                                <a class="btn btn-outline-dark my-md-1 my-sm-0 dropdown-toggle" href="#" id="dropdown01" 
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item my-md-1 my-lg-0 mr-lg-1 dropdown">
+                                <a class="btn btn-outline-dark my-md-1 my-sm-0 dropdown-toggle" href="#" id="dropdown02"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown02">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item my-md-1 my-lg-0 mr-lg-1 dropdown">
+                                <a class="btn btn-outline-dark my-md-1 my-sm-0 dropdown-toggle" href="#" id="dropdown03" 
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown03">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="right-btns my-md-1 my-lg-0">
+                            <button class="btn btn-outline-dark " type="submit">Search</button>
+                            <button class="btn btn-outline-dark " type="submit">Search</button>
+                            <button class="btn btn-outline-dark " type="submit">Search</button>
+                            <button class="btn btn-outline-dark " type="submit">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+                <h1 class="text-dark">Header</h1>
+                <p class="text-dark"> main string</p>
+                <hr/>
+            </div>
+        </main>
+        <footer class="mt-auto py-2">
+            <div class="container">
+                <p class="text-dark"> Footer string</p>
+            </div>
+        </footer>
         <!-- // js block -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
