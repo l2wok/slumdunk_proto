b0b68a6372b53ce9746112fb0e76d31a51051266
Andro <l22wok@gmail.com>
2019-02-26 16:48:43 +0300

    fast menu

--- main.php
+++ main.php
@@ -21,7 +21,143 @@ $ver = 100000 + rand(1, 100000);
         <link rel="stylesheet" href="assets/css/styles_main.css?v=<?php echo $ver;?>">
     </head>
     <body class="d-flex flex-column h-100">
-        
+        <header>
+            <div id="collapseParent" class="bg-color-smoke">
+                <div class="container">
+                    <div class="collapse" id="userPanel" data-parent="#collapseParent">
+                        <div class="row">
+                            <div class="col-md-12 col-lg-8 my-1">
+                                <div class="card">
+                                    <div class="card-header">
+                                        Мы не хотим тратить Ваше время на регистрации или напоминания паролей.
+                                    </div>
+                                    <div class="card-body">
+                                        Для того чтобы сделать заказ достаточно представится используя соц. сети - VK, FB или Instagaram
+                                    </div>
+                                    <div class="card-footer d-flex justify-content-center">
+                                        <button class="btn btn-sm btn-outline-dark mr-2">ВХОД ЧЕРЕЗ <i class="fab fa-vk"></i></button>
+                                        <button class="btn btn-sm btn-outline-dark mr-2">ВХОД ЧЕРЕЗ <i class="fab fa-facebook"></i></button>
+                                        <button class="btn btn-sm btn-outline-dark">ВХОД ЧЕРЕЗ <i class="fab fa-instagram"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-lg-4 my-1">
+                                <div class="card">
+                                    <div class="card-header">
+                                        Ваша корзина пуста!
+                                    </div>
+                                    <div class="card-body">
+                                        Товары не добавлены
+                                    </div>
+                                    <div class="card-footer">
+                                        <button class="btn btn-sm btn-block btn-outline-dark"><i class="fa fa-phone-volume"></i> Сделать заказ по телефону</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="collapse" id="callPanel" data-parent="#collapseParent">
+                        <div class="row">
+                            <div class="w-100 text-center py-1">
+                                <h4>Номер нашего бесплатного телефона 8-800-500-62-63. <a href="tel:8-800-500-62-63" class="btn btn-outline-dark">Позвонить в один клик!</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="collapse" id="searchPanel" data-parent="#collapseParent">
+                        <div class="row">
+                            <div class="w-100 text-center py-1">
+                                <div class="input-group col-sm-12 col-md-4 offset-md-4">
+                                    <input id="search_input" type="text" class="form-control" placeholder="Наименование товара или его категория" aria-describedby="s_input">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="button" id="s_input">Поиск</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-end-shadow-block"></div>
+            </div>
+            <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-color-smoke">
+                <div class="container">
+                    <div class="w-100 top-nav d-flex align-items-center">
+                        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" 
+                                data-target="#j-nav" aria-controls="j-nav" 
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="navbar-collapse offcanvas-collapse" id="j-nav">
+                            <ul class="navbar-nav nav-left">
+                                <li class="nav-item">
+                                    <a class="btn btn-outline-dark" href="/slamdunk/menu.php"><i class="fa fa-home"></i><span class="d-inline d-lg-none d-md-block"> НА&nbsp;ГЛАВНУЮ</span></a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="btn btn-outline-dark dropdown-toggle" href="#" id="cat-col"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> стдСтолбик
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="cat-col">
+                                        <a class="dropdown-item" href="#">Кросовки</a>
+                                        <a class="dropdown-item" href="#">Зимние кросовки</a>
+                                        <a class="dropdown-item" href="#">Кеды</a>
+                                        <a class="dropdown-item" href="#">Сланцы</a>
+                                        <a class="dropdown-item" href="#">Дети</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="btn btn-outline-dark" href="#">МЯЧИ <i class="fab fa-dribbble"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="btn btn-outline-dark" href="#">СКИДКИ</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="ml-auto">
+                            <ul class="navbar-nav nav-right">
+                                <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                                    <!--<div class="btn-group d-none d-md-none d-lg-block" role="group">-->
+                                    <div class="btn-group " role="group">
+                                        <button id="btnGroupDrop1" type="button" class="btn btn-outline-dark dropdown-toggle"
+                                                data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
+                                            Регион
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left" aria-labelledby="btnGroupDrop1">
+                                            <a class="dropdown-item" href="#"><i class="fas fa-location-arrow"></i> Авто-определение</a>
+                                            <a class="dropdown-item" href="#"><i class="fas fa-location-arrow"></i> Выбрать регион</a>
+                                        </div>
+                                    </div>
+                                    <button class="btn btn-outline-dark" type="button"
+                                            data-toggle="collapse" aria-expanded="false" aria-label="Open panel"
+                                            data-target="#searchPanel" aria-controls="searchPanel">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                    <button class="btn btn-outline-dark" type="button"
+                                            data-toggle="collapse" aria-expanded="false" aria-label="Open panel"
+                                            data-target="#callPanel" aria-controls="callPanel">
+                                        <i class="fa fa-phone fa-rotate-90"></i>
+                                    </button>
+                                    <button class="btn btn-outline-dark" type="button"
+                                            data-toggle="collapse" aria-expanded="false" aria-label="Open panel"
+                                            data-target="#userPanel" aria-controls="userPanel">
+                                        <i class="fa fa-user"></i>
+                                    </button>
+                                    <button class="btn btn-outline-dark" type="button">
+                                        <i class="fa fa-shopping-cart"></i>
+                                    </button>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main class="mb-auto">
+            
+        </main>
+        <footer class="bg-color-black">
+            
+        </footer>
         <!-- // js block -->
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
