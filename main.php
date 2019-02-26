c56635be8035dbabbf1229ee79f197220fe6bbe3
Andro <l22wok@gmail.com>
2019-02-26 09:58:05 +0300

    main__ colors css add

--- main.php
+++ main.php
@@ -17,10 +17,20 @@ $ver = 100000 + rand(1, 100000);
         <!-- // css block -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/colors.css?v=<?php echo $ver;?>">
         <link rel="stylesheet" href="assets/css/styles_main.css?v=<?php echo $ver;?>">
     </head>
     <body class="d-flex flex-column h-100">
-        
+        <div class="container">
+            <div class="col-12">
+                <h1>Текущая цветовая гамма</h1>
+            </div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+        </div>
         
         
         <!-- // js block -->
@@ -28,6 +38,6 @@ $ver = 100000 + rand(1, 100000);
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
         <script src="assets/js/current-device.min.js"></script>
-        <script src="assets/js/app_main.js<?php echo $ver;?>"></script>
+        <script src="assets/js/app_main.js?v=<?php echo $ver;?>"></script>
     </body>
 </html>
