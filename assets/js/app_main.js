f17db99d2d92e21e7f5d8f307e6941406ebd7afb
Andro <l22wok@gmail.com>
2019-03-01 19:09:07 +0300

    me ready

--- assets/js/app_main.js
+++ assets/js/app_main.js
@@ -12,38 +12,42 @@ $(function () {
             return false;
         });
     });
+    $('#go-to-top').on('click',function() {
+        $('html, body').animate({scrollTop: 0}, 300);
+        return false;
+    });
 });
 jQuery(function($) {
-    function fix_size() {
-        var images = $('img.item-card-image');
-        images.each(setsize);
-
-        function setsize() {
-            var img = $(this),
-                    img_dom = img.get(0),
-                    container = img.parents('.item-card-image');
-            if (img_dom.complete) {
-                resize();
-            } else
-                img.one('load', resize);
-
-            function resize() {
-                if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
-                    img.width('70%');
-                    img.height('auto');
-                    return;
-                }
-                img.height('70%');
-                img.width('auto');
-                img.css('margin-top', '3rem');
-            }
-        }
-    }
-    $(window).on('resize', fix_size);
-    fix_size();
+//    function fix_size() {
+//        var images = $('img.item-card-image');
+//        images.each(setsize);
+//
+//        function setsize() {
+//            var img = $(this),
+//                    img_dom = img.get(0),
+//                    container = img.parents('.item-card-image');
+//            if (img_dom.complete) {
+//                resize();
+//            } else
+//                img.one('load', resize);
+//
+//            function resize() {
+//                if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+//                    img.width('70%');
+//                    img.height('auto');
+//                    return;
+//                }
+//                img.height('70%');
+//                img.width('auto');
+//                img.css('margin-top', '3rem');
+//            }
+//        }
+//    }
+//    $(window).on('resize', fix_size);
+//    fix_size();
 });
 $(document).ready(function() {
-    setTimeout(showHide,1000);
+    setTimeout(showHide,100);
     
     
     $('[data-toggle="collapse"]').on('click', function() {
